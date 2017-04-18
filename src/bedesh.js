4f43f5d7b9134adbb44c53be3cff410ff370ce85
javid <javid@tarameshgroup.com>
2017-04-18 18:38:16 +0430

    Added opening

--- src/bedesh.js
+++ src/bedesh.js
@@ -5,6 +5,8 @@ const q = require('component-query');
 const doc = require('get-doc');
 const cookie = require('cookie-cutter');
 
+console.log('here is Bedesh');
+
 // date parsing
 const msInOneDay = 1000 * 60 * 60 * 24;
 const today = Number(new Date());
@@ -12,18 +14,22 @@ const today = Number(new Date());
 // root and Dom
 let root = doc && doc.documentElement;
 
-
 let Bedesh = function (options) {
 
 	this.options = extend({}, {
+    daysHidden: 15, // Numbers of days to hide banner after dismissed
+    daysReminder: 90, // Numbers of days to hide banner after downloaded
+    headerContent: '',
+    bodyContent: '',
+    force: false, // always show banner
+    onSubmit: function(){}, // On Submit callback
+    onClose: function() {} // On Dismiss callback
 	}, options || arguments[0] || {});
 
 	const userDismissed = cookie.get('Bedesh-closed');
-	const userSubmited = cookie.get('Bedesh-submited')
+	const userSubmited = cookie.get('Bedesh-submited');
 
 	if (!this.options.force && (userDismissed || userSubmited)) return;
-
-	extend(this, mixins[this.options.store]);
 
 	this.create();
 	this.show();
@@ -34,11 +40,24 @@ Bedesh.prototype = {
 
 	create: function () {
 
+    console.log('creat');
 
 		let modal = doc.createElement('div');
 
-		modal.className = 'Bedesh';
-		modal.innerHTML = '';
+		modal.className = 'bedesh';
+		modal.innerHTML = '<div class="bedesh-overlay bedesh-close"></div>'+
+                        '<div class="bedesh-container">'+
+                          '<div class="bedesh-header">'+
+                            this.options.headerContent +
+                          '</div>'+
+                          '<div class="bedesh-body">'+
+                            '<form action="">'+
+                              this.options.bodyContent +
+                              '<input type="email" placeholder="ایمیل خود را وارد کنید">'+
+                              '<input type="submit" value="ثبت ایمیل">'+
+                            '</form>'+
+                          '</div>'+
+                        '</div>';
 
 		// there isn’t neccessary a body
 		if (doc.body) {
@@ -49,16 +68,19 @@ Bedesh.prototype = {
 			});
 		}
 
-		q('.Bedesh-download', modal).addEventListener('click', this.submit.bind(this), false);
-		q('.Bedesh-close', modal).addEventListener('click', this.close.bind(this), false);
+		// q('.Bedesh-download', modal).addEventListener('click', this.submit.bind(this), false);
+		q('.bedesh-close', modal).addEventListener('click', this.close.bind(this), false);
 	},
 	hide: function () {
-		root.classList.remove('Bedesh-show');
+    console.log('hide');
+		root.classList.remove('bedesh-show');
 	},
 	show: function () {
-		root.classList.add('Bedesh-show');
+    console.log('show');
+		root.classList.add('bedesh-show');
 	},
 	close: function () {
+    console.log('close');
 		this.hide();
 		cookie.set('Bedesh-closed', 'true', {
 			path: '/',
